5e4371389701b5c6e1fb51a032cfc50765444047
CLLlAgOB <clllagob@gmail.com>
2023-10-20 19:06:06 +0200

    Fix 01 for githubacc

--- Terraform/githubacc.tf
+++ Terraform/githubacc.tf
@@ -1,3 +1,7 @@
+provider "yandex" {
+  token = var.token
+}
+
 resource "yandex_iam_service_account" "github_actions" {
   name        = "github-actions"
   description = "Service account for GitHub Actions"
@@ -9,49 +13,40 @@ resource "yandex_iam_service_account_key" "github_actions_key" {
 
 resource "yandex_resourcemanager_folder_iam_binding" "github_actions_role" {
   folder_id = var.folder_id
-  role      = "editor"
-  members   = ["serviceAccount:${yandex_iam_service_account.github_actions.id}"]
 
   bindings {
-    role = "roles/container.clusterAdmin"
-    members = [
-      "serviceAccount:${yandex_iam_service_account.github_actions.id}"
-    ]
+    role    = "editor"
+    members = ["serviceAccount:${yandex_iam_service_account.github_actions.id}"]
   }
 
   bindings {
-    role = "roles/container.developer"
-    members = [
-      "serviceAccount:${yandex_iam_service_account.github_actions.id}"
-    ]
+    role    = "roles/container.clusterAdmin"
+    members = ["serviceAccount:${yandex_iam_service_account.github_actions.id}"]
   }
 
   bindings {
-    role = "roles/container.viewer"
-    members = [
-      "serviceAccount:${yandex_iam_service_account.github_actions.id}"
-    ]
+    role    = "roles/container.developer"
+    members = ["serviceAccount:${yandex_iam_service_account.github_actions.id}"]
   }
 
   bindings {
-    role = "roles/container.admin"
-    members = [
-      "serviceAccount:${yandex_iam_service_account.github_actions.id}"
-    ]
+    role    = "roles/container.viewer"
+    members = ["serviceAccount:${yandex_iam_service_account.github_actions.id}"]
   }
 
   bindings {
-    role = "roles/container.viewer"
-    members = [
-      "serviceAccount:${yandex_iam_service_account.github_actions.id}"
-    ]
+    role    = "roles/container.admin"
+    members = ["serviceAccount:${yandex_iam_service_account.github_actions.id}"]
   }
 
   bindings {
-    role = "roles/container.worker"
-    members = [
-      "serviceAccount:${yandex_iam_service_account.github_actions.id}"
-    ]
+    role    = "roles/container.viewer"
+    members = ["serviceAccount:${yandex_iam_service_account.github_actions.id}"]
+  }
+
+  bindings {
+    role    = "roles/container.worker"
+    members = ["serviceAccount:${yandex_iam_service_account.github_actions.id}"]
   }
 }
 
